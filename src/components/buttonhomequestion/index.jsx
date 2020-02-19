547f1ec369f2101efa16c8aa0c562fa510e0c4e6
Le Montagner <victorlemonta@gmail.com>
2020-02-19 17:41:22 +0100

    Update style of button + import of arrow right .

--- src/components/buttonhomequestion/index.jsx
+++ src/components/buttonhomequestion/index.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 const HomeQuestion = () => (
   <button to="/" className="home-section-question">
-    La question de la semaine >
+    <span>La question de la semaine</span>
+    <img src="../images/Polygon_1.png"/>
   </button>
 );
 
