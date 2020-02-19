caec91f8b1f789e1edb1a68670d93c192a6060cc
Le Montagner <victorlemonta@gmail.com>
2020-02-19 16:58:19 +0100

    Init of the home page .

---
+++ src/components/buttonhomequestion/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomeQuestion = () => (
+  <button to="/">
+    La question de la semaine >
+  </button>
+);
+
+export default HomeQuestion;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import HomeQuestion from '../../components/buttonhomequestion/index';
+// import image_1 from '../../../public/images/smile-décli-ux@2x.png';
+// import image_2 from '../../../public/images/smile-décli-webmarket@2x.png';
+// import badge from '../../../public/images/Card.png';
 
-const Home = () => <h1>Home</h1>;
+const Home = () => 
+    <section class="home-section">
+        <div class="home-section-image-1"></div>
+        <div class="home-section-image-2"></div>
+        <div class="home-section-badge">
+            <div class="home-section-badge-image"></div>
+            <h1>Badge pour émarger</h1>
+        </div>
+        <HomeQuestion class="home-section-question"></HomeQuestion>
+    </section>;
 
 export default Home;
